539d7f156ad7c19d0207b460885c478a22e77ebe
Tomas <no>
2024-11-05 10:23:09 -0300

    Done Project

--- src/Bubble.java
+++ src/Bubble.java
@@ -1,12 +1,48 @@
-/** THE ENTIRE CLASS IS INSIDE A COMMENT RIGHT NOW. TO ENABLE THE CLASS, DELETE THIS LINE AND THE LAST ONE.
+
 
 class Bubble {
 
-    //MISSING MANY INSTANCE VARIABLES
+ /** The sketch that the ball is inside */
+ private Sketch s;
+ /** the radius of the ball */
+ private float radius;
+ private float x;
+ private float y;
+ /** The number of pixels the ball moves right per frame */
+ private float xSpeed;
+ /** The number of pixels the ball moves down per frame */
+ private float ySpeed;
+ /** The color of the inside of the ball */
+ private int fillColor;
+ /** The color of the outside of the ball */
+ private int borderColor;
 
-    //MISSING CONSTRUCTORS
+ /** Empty constructor to keep the defaults. Only sets up our link. */
+ public Bubble(Sketch sketch) {
+     s = sketch;
+     radius = 20;
+     x = 100;
+     y = 100;
+     xSpeed = 2;
+     ySpeed = -1;
+     fillColor = s.color(255, 255, 255,0);
+     borderColor = s.color(0, 0, 0);
+     
+ }
 
-    // accessors for the radius, diameter, x, and y values 
+ /** Fully specified constructor to allow changes to size, position, speed */
+ /** Does NOT allow changing color! Need to use setColors() for that. */
+ public Bubble(Sketch sketch, float radius, float x, float y, float xspeed, float yspeed) {
+     this.s = sketch;
+     this.radius = radius;
+     this.x = x;
+     this.y = y;
+     this.xSpeed = xspeed;
+     this.ySpeed = yspeed;
+     fillColor = s.color(255, 255, 255,120);
+     borderColor = s.color(0, 0, 0);
+ }
+
     public float getRadius() {
         return radius;
     }
@@ -22,13 +58,13 @@ class Bubble {
     public float getY() {
         return y;
     }
-
+/**This method draws the bubble on the screen */
     public void draw() {
         s.stroke(borderColor);
         s.fill(fillColor);
         s.circle(x, y, radius*2);
     }
-
+/**This method determines the movement of the bubbles */
     public void move() {
         x = x + xSpeed;
         y = y + ySpeed;
@@ -44,4 +80,3 @@ class Bubble {
     }
 
 }
- **/
--- src/Snowflake.java
+++ src/Snowflake.java
@@ -1,16 +1,88 @@
-/** THE ENTIRE CLASS IS INSIDE A COMMENT RIGHT NOW. TO ENABLE THE CLASS, 
- * DELETE THESE LINE AND THE LAST ONE IN THE FILE.
 
 class Snowflake {
-    // ONLY THE DRAW METHOD IS INCLUDED. YOU MUST FINISH THE REST.
   
+ /** The sketch that the snowflake is inside */
+ private Sketch s;
+ /** the radius of the snowflake */
+ private float radius;
+ private float x;
+ private float y;
+ /** The number of pixels the ball moves right per frame */
+ private float xSpeed;
+ /** The number of pixels the ball moves down per frame */
+ private float ySpeed;
 
-    public void draw() {
-        line(x+radius,y,x-radius,y);
-        line(x,y+radius,x,y-radius);
-        line(x+radius*.707,y+radius*.707,x-radius*.707,y-radius*.707);
-        line(x+radius*.707,y-radius*.707,x-radius*.707,y+radius*.707);
+
+
+ 
+
+ /** Empty constructor to keep the defaults. Only sets up our link. */
+ public Snowflake(Sketch sketch) {
+     s = sketch;
+     radius = 20;
+     x = 100;
+     y = 100;
+     xSpeed = 2;
+     ySpeed = -1;
+    
+    
+     
+ }
+
+ /** Fully specified constructor to allow changes to size, position, speed */
+ /** Does NOT allow changing color! Need to use setColors() for that. */
+ public Snowflake(Sketch sketch, float radius, float x, float y, float xspeed, float yspeed) {
+     this.s = sketch;
+     this.radius = radius;
+     this.x = x;
+     this.y = y;
+     this.xSpeed = xspeed;
+     this.ySpeed = yspeed;
+    
+ 
+ }
+
+    public float getRadius() {
+        return radius;
     }
+
+    public float getDiameter() {
+        return radius * 2;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+/** This method draws the snowflake determining where the lines are drawn */
+    public void draw() {
+        s.stroke(s.color(255,255,255));
+        s.line(x+radius,y,x-radius,y);
+        s.line(x,y+radius,x,y-radius);
+        s.line(x+radius*.707f,y+radius*.707f,x-radius*.707f,y-radius*.707f);
+        s.line(x+radius*.707f,y-radius*.707f,x-radius*.707f,y+radius*.707f);
+    }
+
+
+/** This method moves the snowflakes. It determines how they are to move around on the screen. */
+    public void move() {
+        x = x + xSpeed;
+        y = y + ySpeed;
+        if (x > s.width - radius) {
+            x = radius;
+        } else if(x < radius) {
+            x = s.width - radius;
+        } else if (y > s.height - radius) {
+            y = radius;
+        } else if (y < radius) {
+            y = s.height - radius;
+        }
+    }
+
+
+
 }
 
-**/
